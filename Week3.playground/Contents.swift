a612637093f01432794ab879ca49f4bb68fdab68
Juan Manuel Mougan <juanmougan@gmail.com>
2016-01-13 22:41:19 -0300

    Added material to practice for week 3

--- Week3.playground/Contents.swift
+++ Week3.playground/Contents.swift
@@ -2,4 +2,63 @@
 
 import UIKit
 
-var str = "Hello, playground"
+// Optionals
+var isString: String? = "Hello, playground"    // not sure if it's a String... could be nil!
+isString!.characters.count                     // unwrap to string
+
+var notString: String?
+// notString!.characters.count     // error... is nil!
+
+// Functions as first class citizens
+func sayHelloTo(person: String) -> String {
+    return "Hello, \(person)!"
+}
+
+sayHelloTo("Juan")
+
+let myFunction = sayHelloTo
+myFunction("Natalia")
+
+// Closure structure
+//    { (parameters) -> return type in
+//        statements
+//    }
+
+var myFirstClosure = {
+    (str: String) -> String in
+    return str.uppercaseString
+}
+
+myFirstClosure("Some MixeD CaSe StRIng")
+
+// Classes and Structs
+class Person {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+
+var p = Person(name: "Juan")
+var p2 = p
+
+p.name = "Natalia"
+
+p.name          // Changed to Natalia
+p2.name         // Also changed to Natalia
+
+// Structs behave like value types, not like references
+struct CoolerPerson {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+
+var coolerP = CoolerPerson(name: "Juan")
+var coolerP2 = coolerP
+
+coolerP.name = "Natalia"
+
+coolerP.name          // Changed to Natalia
+coolerP2.name         // Still Juan! Because isn't a reference...
