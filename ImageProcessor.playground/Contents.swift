a054bf577c6555e1b910b0a8cf895da718838675
Juan Manuel Mougan <juanmougan@gmail.com>
2017-07-27 22:43:53 -0300

    Playground for final assignment

---
+++ ImageProcessor.playground/Contents.swift
@@ -0,0 +1,15 @@
+// Your Playground code should fulfill the following project requirements:
+
+// Be able to apply a filter formula to each pixel of the image.
+// The formula should have parameters that can be modified so that the filter can have a small or large effect on the image.
+// Be able to apply several different filters to the image, in a specific order (e.g. a ‘pipeline of filters’). These could be different formulas (e.g. brightness vs. contrast) or could be the same formula with different parameters.
+// Have some method or interface to apply default filter formulas and parameters that can be accessed by name.
+
+// The indexing formula is:
+//  
+
+import UIKit
+
+let image = UIImage(named: "sample")
+
+// Process the image!
